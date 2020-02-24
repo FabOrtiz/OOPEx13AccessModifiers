ecca9e2c282be3382bb3e22a1e21e1e1f519168d
FabOrtiz <59565838+FabOrtiz@users.noreply.github.com>
2020-02-24 10:02:45 -0400

    Class demo

---
+++ AccessModifiersDriverCar/VehicleOperation/Car.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleOperation
+{
+    internal class Car
+    {
+
+        //Deteils on the operation of a car
+        //Lets abstract or hide away these details from another classes
+        #region [Private Methods]
+
+        private void InjectedFuel()
+        {
+
+        }
+
+        private void Ignition()
+        {
+
+        }
+
+        private void IncreaseAirIntake()
+        {
+
+        }
+
+        private void DecreaseAirIntake()
+        {
+
+        }
+
+        private void IncreaseFuelIntake()
+        {
+
+        }
+
+        private void DecreaseFuelIntake()
+        {
+
+        }
+
+        private void IncreaseBrakelinePressure()
+        {
+
+        }
+
+        private void DecreaseBrakelinePressure()
+        {
+
+        }
+
+
+        #endregion
+
+        #region [Public Methods]
+
+        public void StartEngine()
+        {
+            this.InjectedFuel();
+            this.IncreaseAirIntake();
+            this.Ignition();
+        }
+
+        public void SpeedUp()
+        {
+            this.IncreaseAirIntake();
+            this.IncreaseFuelIntake();
+        }
+
+        public void SlowDown()
+        {
+            this.DecreaseAirIntake();
+            this.DecreaseFuelIntake();
+        }
+
+        public void ApplyBrake()
+        {
+            SlowDown();
+            this.IncreaseBrakelinePressure();
+        }
+
+        public void Stop()
+        {
+            ApplyBrake();
+            HoldBrake();
+        }
+
+        public void HoldBrake()
+        {
+
+        }
+
+        #endregion
+    }
+}
---
+++ AccessModifiersDriverCar/VehicleOperation/Driver.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleOperation
+{
+    public class Driver
+    {
+        public void OperateCar()
+        {
+            Car car = new Car();
+
+            GetKeysOutOfPocket();
+            OpenDoor();
+            SitDown();
+
+            car.StartEngine();
+            car.SpeedUp();
+            car.ApplyBrake();
+            car.HoldBrake();
+        }
+
+        private void OpenDoor() { }
+        private void SitDown() { }
+        private void GetKeysOutOfPocket() { }
+    }
+}
